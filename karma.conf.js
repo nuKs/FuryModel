d08f6fc7d79e72515007e276587360dca5d12e29
Thibault PIRONT <gse.nuks@gmail.com>
2015-02-26 00:57:42 +0100

    chore: allow sinonjs mocking

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'sinon'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'dist/when.js',
       'src/**/*.js',
       'test/**/*Spec.js'
     ],
